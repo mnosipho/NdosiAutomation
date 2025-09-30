d3a1c18c113e7065f556c109200585d21b207612
mac <mac@macs-MacBook-Pro.local>
2025-09-30 12:39:40 -0700

    Added a faker data generater class

---
+++ src/test/java/Utils/FakerDataGenerator.java
@@ -0,0 +1,14 @@
+package Utils;
+
+import com.github.javafaker.Faker;
+
+public class FakerDataGenerator {
+
+    static Faker faker = new Faker();
+
+    public static String firstName =faker.name().firstName();
+    public static String address =faker.address().cityName();
+    public static String surname =faker.name().lastName();
+}
+
+
